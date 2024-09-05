4b2fbcfca9cfed73a8bf4a9b692a34b95742aee8
Joink <755969798@qq.com>
2024-09-06 04:28:46 +0800

    修改了test文件

--- test.cpp
+++ test.cpp
@@ -43,10 +43,8 @@ int main()
     Player::addcard(new DefendCard("闪",{"闪"},3,1,1,5));
     Player::addcard(new DefendCard("闪",{"闪"},3,1,1,5));
     Player::addcard(new DefendCard("闪",{"闪"},3,1,1,5));
-    //Player::addcard(new AttackCard("闪电",{"闪电","造成6X2点伤害"},1,2,3,6,2));
-    //Player::addcard(new AttackCard("南蛮入侵",{"南蛮入侵","造成5X3点伤害"},2,3,4,5,3));
     EnemyObject tenemyobject;
-    Enemy tenemy=Enemy("汪希",{"汪希"},100);
+    Enemy tenemy=Enemy("于景一",{"于景一"},100);
     tenemy.state.defense=100;
     tenemy.addintention(new EnemyIntentionAttack({"攻击20点"},20,1));
     tenemy.addintention(new EnemyIntentionAttack({"攻击10X3点"},10,3));
@@ -55,20 +53,15 @@ int main()
     tenemy.addintention(new EnemyIntentionDefend({"防御100点"},100));
     tenemy.addintention(new EnemyIntentionStrengthen({"力量30点"},30));
     tenemy.addintention(new EnemyIntentionGiveCard({"施加混乱"},{new Card("混乱",{"混乱"},0,99,1),new Card("混乱",{"混乱"},0,99,1),new Card("混乱",{"混乱"},0,99,1)}));
-    /* rooms.push_back(Room("客厅(1F)",0,1,2,3,4));
-    rooms.push_back(Room("练功房",1,-1,0,-1,-1));
-    rooms.push_back(Room("书房",2,0,-1,-1,-1));
-    rooms.push_back(Room("厕所",3,-1,-1,-1,0));
-    rooms.push_back(Room("卧室",4,-1,-1,0,-1));
+    getMapList();
     rooms[0].addobject(new EnemyObject("人",4,4,tenemy));
     ChangeHaveCard juedou=ChangeHaveCard("决斗",{"决斗","对敌人造成6点","伤害同时将一张","决斗加入你的","摸牌堆"},5,0,4);
     juedou.setattack(6,1);
     juedou.changehavecard.push_back(&juedou);
     std::vector<Goods*> goodss={new CardGoods(&juedou,50,10),new CardGoods(new StrengthenCard("酒",{"酒"},0,1,4,10),100,5),new CardGoods(new DrawCard("无中生有",{"无中生有","摸2张牌"},4,0,4,2),100,5),new PropGoods(new Prop("灵石",{"似乎蕴含着一些神奇的力量"},"red"),100,1)};
     rooms[0].addobject(new StoreObject("黑市",4,0,goodss));
-    rooms[0].addobject(new WallObject("墙","撞墙了，好痛"),{{10,0},{10,1},{10,2},{10,3},{10,4},{10,5},{10,6}});  */
-    //datamap1();
-    getMapList();
+    rooms[0].addobject(new WallObject("墙","撞墙了，好痛"),{{10,0},{10,1},{10,2},{10,3},{10,4},{10,5},{10,6}});  
+    
     playerCurrentRoom=0;
     printmap();
     printsmallmap();
